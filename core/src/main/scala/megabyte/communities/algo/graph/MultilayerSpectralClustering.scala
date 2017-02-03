5ca042324adbb14447f737fbb9b79407bdd3fb3a
Denis Mekhanikov <dmekhanikov@gmail.com>
2017-02-03 18:53:20 +0800

    add more logging to multilayer clustering

--- core/src/main/scala/megabyte/communities/algo/graph/MultilayerSpectralClustering.scala
+++ core/src/main/scala/megabyte/communities/algo/graph/MultilayerSpectralClustering.scala
@@ -12,33 +12,35 @@ private class MultilayerSpectralClustering
 
 object MultilayerSpectralClustering {
 
-  private val logger = Logger[MultilayerSpectralClustering]
+  private val LOG = Logger[MultilayerSpectralClustering]
 
   // alpha - coefficient in the objective function. Importance of layers to be close on the manifold.
   def getClustering(graphs: Seq[Graph[Long, Edge]], k: Int, alpha: Double): Map[Long, Int] = {
     val numeration = numerateNodes(graphs.head)
-    val adjMatrices = graphs.map { g =>
+    val adjMatrices = graphs.par.map { g =>
       val renumG = applyNumeration(g, numeration)
       adjacencyMatrix(renumG)
     }
-    val renumClustering = getClustering(adjMatrices, k, alpha)
+    val renumClustering = getClustering(adjMatrices.seq, k, alpha)
     Map(numeration.zip(renumClustering): _*)
   }
 
   def getClustering(adjMatrices: Seq[DoubleMatrix], k: Int, alpha: Double): Seq[Int] = {
-    logger.info(s"Starting multilayer clustering computation")
+    LOG.info(s"Starting multilayer clustering computation")
     val lSyms = adjMatrices.map(symLaplacian)
     val us = lSyms.zipWithIndex.map { case (l, i) =>
-      logger.info(s"Starting processing layer #${i + 1}")
+      LOG.info(s"Starting processing layer #${i + 1}")
       val u = toEigenspace(l, k).normRowsI()
-      logger.info(s"${i + 1} / ${adjMatrices.size} layers processed")
+      LOG.info(s"${i + 1} / ${adjMatrices.size} layers processed")
       u
     }
+    LOG.info("Building modified Laplacian matrix")
     val n = adjMatrices.head.rows
     val lMod = new DoubleMatrix(n, n)
     lSyms.zip(us).foreach { case (li, ui) =>
       lMod += (li -= ((ui * ui.transpose()) *= alpha))
     }
+    LOG.info("Running spectral clustering on the modified Laplacian matrix")
     val u = toEigenspace(lMod, k).normRowsI()
     XMeans.getClustering(u)
   }
