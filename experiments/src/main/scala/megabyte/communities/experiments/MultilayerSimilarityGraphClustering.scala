051a7159103c565a464c534b2f6fe99a6d9f0149
Denis Mekhanikov <dmekhanikov@gmail.com>
2017-02-17 16:39:57 +0800

    add multilayer similarity graph clustering

---
+++ experiments/src/main/scala/megabyte/communities/experiments/MultilayerSimilarityGraphClustering.scala
@@ -0,0 +1,39 @@
+package megabyte.communities.experiments
+
+import java.io.File
+
+import com.typesafe.scalalogging.Logger
+import megabyte.communities.algo.graph.MultilayerSpectralClustering
+import megabyte.communities.experiments.ClusteringUtil._
+import megabyte.communities.experiments.config.ExperimentConfig
+
+private class MultilayerSimilarityGraphClustering
+
+object MultilayerSimilarityGraphClustering {
+
+  private val LOG = Logger[MultilayerSimilarityGraphClustering]
+
+  private val BASE_DIR = ExperimentConfig.config.baseDir
+  private val CITY = ExperimentConfig.config.city
+  private val GRAPHS_DIR = new File(s"$BASE_DIR/$CITY/graphs/similarity")
+
+  private val INPUT_FILES = Seq(
+    "foursquare.csv",
+    "twitter.csv",
+    "instagram.csv")
+
+  def main(args: Array[String]): Unit = {
+    val adjs = INPUT_FILES.par.map { fileName =>
+      readDataFile(new File(GRAPHS_DIR, fileName))
+    }.seq
+    val clustering = MultilayerSpectralClustering.getClustering(adjs, 10, 0.2)
+    logStats(clustering)
+  }
+
+  private def logStats(clustering: Seq[Int]): Unit = {
+    val invClustering = clustering.groupBy(identity)
+    val clustersNum = clustering.max + 1
+    LOG.info(s"number of clusters: $clustersNum")
+    LOG.info(s"sizes:" + invClustering.foldLeft("") { (s, cluster) => s"$s ${cluster._2.size}" })
+  }
+}
