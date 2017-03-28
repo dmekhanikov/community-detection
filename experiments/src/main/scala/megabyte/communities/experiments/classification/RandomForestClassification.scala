597f3d8d2dac47f83aa612e36b1ab40f3004747a
Denis Mekhanikov <dmekhanikov@gmail.com>
2017-03-28 18:46:14 +0800

    print detailed statistics after random forest classification

--- experiments/src/main/scala/megabyte/communities/experiments/classification/RandomForestClassification.scala
+++ experiments/src/main/scala/megabyte/communities/experiments/classification/RandomForestClassification.scala
@@ -22,14 +22,36 @@ object RandomForestClassification {
     val trainData = IO.readInstances(trainFile)
     val testData = IO.readInstances(testFile)
     trainData.setClassIndex(trainData.numAttributes() - 1)
-    evaluate(trainData, testData)
+    testData.setClassIndex(trainData.numAttributes() - 1)
+    val evaluation = getEvaluation(trainData, testData)
+    printDetailedStats(evaluation)
   }
 
   def evaluate(trainData: Instances, testData: Instances): Double = {
+    val evaluation = getEvaluation(trainData, testData)
+    evaluation.unweightedMacroFmeasure()
+  }
+
+  private def getEvaluation(trainData: Instances, testData: Instances): Evaluation = {
     val randomForest = new RandomForest()
     randomForest.buildClassifier(trainData)
     val evaluation = new Evaluation(trainData)
     evaluation.evaluateModel(randomForest, testData)
-    evaluation.unweightedMacroFmeasure()
+    evaluation
+  }
+
+  def printDetailedStats(evaluation: Evaluation): Unit = {
+    LOG.info(evaluation.toSummaryString("Results", true))
+    LOG.info(evaluation.toClassDetailsString)
+    LOG.info("Micro F-measure = " + evaluation.unweightedMicroFmeasure())
+    LOG.info("Macro F-measure = " + evaluation.unweightedMacroFmeasure())
+    LOG.info("Results For Class -1-")
+    LOG.info("Precision = " + evaluation.precision(0))
+    LOG.info("Recall = " + evaluation.recall(0))
+    LOG.info("F-measure = " + evaluation.fMeasure(0))
+    LOG.info("Results For Class -2-")
+    LOG.info("Precision = " + evaluation.precision(1))
+    LOG.info("Recall = " + evaluation.recall(1))
+    LOG.info("F-measure = " + evaluation.fMeasure(1))
   }
 }
