de1fcc65146034c49b740e9e2f5df6306d96a442
Denis Mekhanikov <dmekhanikov@gmail.com>
2017-05-14 12:24:14 +0800

    add SubscribersSelector

---
+++ experiments/src/main/scala/megabyte/communities/experiments/transformer/SubscribersSelector.scala
@@ -0,0 +1,29 @@
+package megabyte.communities.experiments.transformer
+
+import java.io.File
+
+import megabyte.communities.experiments.config.ExperimentConfig.config._
+import megabyte.communities.experiments.dao.MongoDAO
+import megabyte.communities.util.IO
+
+import scala.collection.JavaConversions._
+
+object SubscribersSelector {
+
+  val portalsFileName = "malePortals.txt"
+  val subscribersFileName = "maleSubscribers.txt"
+
+  def main(args: Array[String]): Unit = {
+    val portalIdsFile = new File(baseDir, portalsFileName)
+    val portalIds = IO.readLines(portalIdsFile).map(_.toLong).toSet
+    val dao = new MongoDAO(MongoDAO.CRAWLER_DB)
+    val userIds = dao.getUserIds(network)
+    val subscribers = userIds
+      .filter(id =>
+        dao.getFriends("twitter", id)
+          .exists(portalIds.contains(_)))
+      .map(_.toString)
+    val outFile = new File(baseDir, subscribersFileName)
+    IO.writeLines(subscribers, outFile)
+  }
+}
